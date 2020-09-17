6e9efc4e90d5c37c17eff4e6e2e6da634e0fd64a
catcherindarah <22rray@athenian.org>
2020-09-17 21:15:31 +0000

    i finished the assignment

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-
+        System.out.print("What is your name? ");
+        String name = scan.next();
+        System.out.println("Nice to meet you, " + name);
+        
+        System.out.print("What is your favorite color? ");
+        String color = scan.next();
+        System.out.println("Oh I like " + color + "too!");
         scan.close();
     }
 }
